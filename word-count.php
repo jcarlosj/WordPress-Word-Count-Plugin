0524c4b0461fb7fc8e3e3550897e90c8302dbff5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-02-11 13:33:32 -0500

    [.] Plugin: Define 'Word Count'

---
+++ word-count.php
@@ -0,0 +1,8 @@
+<?php 
+    /**
+     * Plugin Name: Word Count
+     * Description: This plugin analyzes the content to determine: number of words, number of characters and estimated reading time of a post.
+     * Version: 1.0
+     * Author: Juan Carlos Jiménez Gutiérrez
+     * Author URI: https://github.com/jcarlosj
+     */
