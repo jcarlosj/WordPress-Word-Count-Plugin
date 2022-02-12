4190c2f4e3a025cf80cc20d1dd36e492a129ddcd
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-02-12 13:59:56 -0500

    [+] word-count.php: Agrega campo a formulario de configuracion para funcionalidad del plugin

--- word-count.php
+++ word-count.php
@@ -15,25 +15,77 @@
             function __construct() {
                 # Agrega un Callback a un Hook 'admin_menu'
                 add_action( 'admin_menu', [ $this, 'addPluginAccessLinkToSettingsMenu' ] );
+                add_action( 'admin_init', [ $this, 'settings' ] );
             }
 
-            # Define la funcionalidad
+            function settings() {
+
+                # Agregue nueva sección a página de configuración.
+                add_settings_section(
+                    'wcp_settings_section',     # $id           / ID único como nombre de la seccion
+                    null,                       # $title        / Título de la sección (En este caso no lo requerimos)
+                    null,                       # $callback     / Callback a ejecutar (En este caso no lo requerimos)
+                    'wcp-settings-page'         # $page         / Nombre de la página a la que se asociará la sección
+                );
+
+                # Agrega nuevo campo a una sección de página de configuración.
+                add_settings_field(
+                    'wcp_location',                     # $id           / ID único como nombre del campo
+                    'Display location',                 # $title        / Label para el campo
+                    [ $this, 'locationField_html' ],    # $callback     / Callback a ejecutar
+                    'wcp-settings-page',                # $page         / Nombre de la página a la que se asociará el campo
+                    'wcp_settings_section'              # $section      / Nombre de la sección a la que se asociará el campo
+                    # $args     / Argumentos adicionales utilizados al generar el campo ('label_for', 'class').
+                );
+
+                # Agrega nuevo campo asociandolo un grupo y registrar datos en la tabla wp_options
+                register_setting(
+                    'wcp-main-section',                  # $option_group / ID único como nombre del grupo
+                    'wcp_location',                     # $option_name  / ID único como nombre del campo
+                    [                                   # $args         / Describe configuracion para los datos, asignar valores predeterminados
+                        'sanitize_callback' => 'sanitize_text_field',
+                        'default' => '0'
+                    ] 
+                );
+            }
+
+            # Despliega campo personalizado en página de configuración
+            function locationField_html() {
+                ?>
+                    <select name="wcp_location">
+                        <option value="0">Beginning of post</option>
+                        <option value="1">End of post</option>
+                    </select>
+                <?php
+            }
+
+            # FrontEnd (Admin): Agrega enlace de acceso a la página desde el menu de configuración de WP
             function addPluginAccessLinkToSettingsMenu() {
 
                 # Agrega enlace al submenú del menu de Configuración y vincula con un FrontEnd en el ADMIN.
                 add_options_page(
-                    'Word Count Settings',      # $page_title / Se despliega en la pesta
-                    'Word Count',               # $menu_title / Nombre del enlace en el menu
-                    'manage_options',           # $capability / Permiso que permite ver, editar y guardar opciones para el sitio web
-                    'word-count-setting-page',  # $menu_slug
-                    [ $this, 'pageWordCountSettings' ]     # $function
+                    'Word Count Settings',          # $page_title   / Título de la página que se despliega en la pestaña del navegador
+                    'Word Count',                   # $menu_title   / Nombre del item de menú que se desplegará en el FrontEnd (Admin)
+                    'manage_options',               # $capability   / Permiso que permite ver, editar y guardar opciones para el sitio web (Admin)
+                    'wcp-settings-page',            # $menu_slug    / Nombre del Slug de página (URL: Admin)
+                    [ $this, 'settingsPage_html' ]  # $function     / Callback a ejecutar
                 );
             }
 
-            # FrontEnd: Despliega pagina de Configuracion del Plugin dentro del ADMIN
-            function pageWordCountSettings() {
+            # FrontEnd (Admin): Despliega pagina de Configuracion del Plugin dentro del ADMIN
+            function settingsPage_html() {
                 ?>
-                    <h1>Page</h1>
+                    <div class="wrap">
+                        <h1>Word Count Settings</h1>
+                        <form action="options.php" method="POST">
+                            <?php
+                                settings_fields( 'wcp-main-section' );               # WP agrega campos de salida nonce, action y option_page para una página de configuración (permite además salvar los cambios del campo)
+                                do_settings_sections( 'wcp-settings-page' );  # WP mostrará automáticamente cualquier seccion y los campos creados para esta pagina
+                                submit_button();
+                            ?>
+                        </form>
+                    </div>
+
                 <?php
             }
         }
