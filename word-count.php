76b5bd6e07f3cddba5f57254cfd7ce9981ffcb10
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-02-12 14:09:56 -0500

    [+] word-count.php: Obtiene valores de la BD y las compara para marcar el campo con el valor seleccionado

--- word-count.php
+++ word-count.php
@@ -51,10 +51,12 @@
 
             # Despliega campo personalizado en página de configuración
             function locationField_html() {
+                # get_option    / Recupera un valor de opción basado en un nombre de opción
+                # selected      / Compara los dos primeros argumentos y, si son idénticos, los marca como seleccionados.
                 ?>
                     <select name="wcp_location">
-                        <option value="0">Beginning of post</option>
-                        <option value="1">End of post</option>
+                        <option value="0" <?php selected( get_option( 'wcp_location' ), '0' ); ?>>Beginning of post</option>
+                        <option value="1" <?php selected( get_option( 'wcp_location' ), '1' ); ?>>End of post</option>
                     </select>
                 <?php
             }
